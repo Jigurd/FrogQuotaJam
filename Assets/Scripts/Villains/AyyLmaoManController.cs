61ce73906e9c97944ee7022082bdcdee6572e796
Mathias Hareide <mathiashareide4497@gmail.com>
2020-04-04 22:08:03 +0200

    idk

---
+++ Assets/Scripts/Villains/AyyLmaoManController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AyyLmaoManController : MonoBehaviour
+{
+    [SerializeField] private float _probeTimer = 0;
+    [SerializeField] private float _speed = 0;
+    [SerializeField] private GameObject[] _victims = null;
+
+    private float _probeTime = 3;
+    private bool _lookingForVictim = true;
+    private Vector3 _pointA;
+    private Vector3 _pointB;
+    private Vector3 _targetPoint;
+    private GameObject _victim;
+
+    private float _distanceBetweenEnemyAndT(Vector3 v3) 
+    {
+        return Vector3.Distance(transform.position, v3);
+    }
+
+    private void Awake()
+    {
+        _pointA = new Vector3(transform.position.x + 6, transform.position.y, transform.position.z);
+        _pointB = new Vector3(transform.position.x - 6, transform.position.y, transform.position.z);
+        _targetPoint = _pointA;
+    }
+    private void Update()
+    {
+        Move();
+
+        if (!_lookingForVictim)
+            Probe();
+    }
+
+    private void Move()
+    {
+        if (!_lookingForVictim)
+            return;
+
+        if (_targetPoint == _pointA)
+            transform.position += Vector3.right * _speed * Time.deltaTime;
+        else if (_targetPoint == _pointB)
+            transform.position += -Vector3.right * _speed * Time.deltaTime;
+
+        if (_distanceBetweenEnemyAndT(_pointA) <= 2)
+        {
+            _targetPoint = _pointB;
+        }
+        else if (_distanceBetweenEnemyAndT(_pointB) <= 2)
+        {
+            _targetPoint = _pointA;
+        }
+                
+        _probeTimer -= Time.deltaTime;
+        if (_probeTimer <= 0)
+        {
+            _victim = _victims[Random.Range(0, _victims.Length - 1)].gameObject;
+
+            _lookingForVictim = false;
+            print("Came to earth in a silver chrome U.F.O, to probe your ass and see how deep it can go.");
+
+            _probeTimer = _probeTime;
+        }
+    }
+    private void Probe()
+    {
+
+
+        /*
+        When I show up, I'm just a little SECTOID.
+        But later on, I become a MECHTOID.
+        You may spot me, then I move free.
+        Then I land a crit on your rookie.
+        Can't see me, can't shoot me,
+        but I'm fucked when you try to flank me.
+        
+        Came to earth in a silver chrome U.F.O,
+        to probe your ass and see how deep it can go.
+        Gain a boost when I merge my mind with my bro,
+        AAAAAAAAAAAAAAAY LMAO. (2x)
+        */
+    }
+}
--- Assets/Scripts/Villains/MuggerManController.cs
+++ Assets/Scripts/Villains/MuggerManController.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class MuggerManController : MonoBehaviour
 {
-    [SerializeField] private bool _canPerformAction = true;
-    [SerializeField] private float _followDistance;
-    [SerializeField] private float _attackDistance;
+    [SerializeField] private float _followDistance = 0;
+    [SerializeField] private float _attackDistance = 0;
     // After attacking it take this long before the enemy can do move/attack
-    [SerializeField] private float _actionRecovery;
-    [SerializeField] private Vector3 _movementSpeed = new Vector3(2, 0, 0);
-    [SerializeField] private Transform _playerTransform;
-    [SerializeField] private Transform _muggeeTransform;
-    // GameObject(s) whomst get mugged
-    [SerializeField] private GameObject[] _memes;
+    [SerializeField] private float _actionRecovery = 0;
+    [SerializeField] private Transform _playerTransform = null;
+    [SerializeField] private Transform _muggeeTransform = null;
+    [SerializeField] private int _speed = 2;
+    
+    private bool _canPerformAction = true;
+    private int _movementSpeed;
 
     private void Update()
     {
@@ -22,10 +22,6 @@ public class MuggerManController : MonoBehaviour
         //print(Vector2.Distance(transform.position, _playerTransform.position));
     }
 
-    private void DoTask()
-    {
-
-    }
     private void TaskFinder()
     {
         /*
@@ -39,58 +35,52 @@ public class MuggerManController : MonoBehaviour
 
         if (distanceBetweenMuggerAndPlayer <= _attackDistance)
         {
-            // if the plaeyr is within attack-range attack them
+            // if the player is within attack-range attack them
 
             AttackPlayer();
-            print("1");
         }
         else if (distanceBetweenMuggerAndPlayer <= _followDistance)
         {
             // if the player is within follow-range chase them
 
             FindTarget(_playerTransform);
-            transform.position += _movementSpeed * Time.deltaTime;
-            print("2");
+            transform.position += Vector3.right * _movementSpeed * Time.deltaTime;
         }
         else if (distanceBetweenMuggerAndPlayer > _followDistance && Vector2.Distance(transform.position, _muggeeTransform.position) > 1.0f)
         {
             // If the player is too far away -> move towards the meme to fuck with
 
             FindTarget(_muggeeTransform);
-            transform.position += _movementSpeed * Time.deltaTime;
+            transform.position += Vector3.right * _movementSpeed * Time.deltaTime;
             
-            print("3");
         }
         else if (Vector2.Distance(transform.position, _muggeeTransform.position) <= 1.0f)
         {
             // if the whatever you are trying to fuck with is within range -> fuck with it
-            print("4");
+
+            DoTask();
         }
     }
     private void FindTarget(Transform t)
     {
-        // there is a better way to do this for
-
-        float dotMeme = Vector2.Dot(transform.right, t.position);
-
-        if (dotMeme == 0)
+        if (t == null)
         {
-            print("UMMM");
-
-            _movementSpeed = new Vector3(0, 0, 0);
+            print("bad memes :c");
+            return;
         }
-        else if (dotMeme > 0)
-        {
-            //print("Player is to the right!");
 
-            _movementSpeed = new Vector3(2, 0);
+        if (t.position.x - transform.position.x > 0)
+        {
+            _movementSpeed = _speed;
         }
         else
         {
-            //print("Player is to the left!");
-
-            _movementSpeed = new Vector3(-2, 0);
+            _movementSpeed = -_speed;
         }
+    }
+    private void DoTask()
+    {
+        print("MUGGIN");
     }
     private void AttackPlayer()
     {
