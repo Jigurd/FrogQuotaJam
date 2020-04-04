04986ea0a8b0bf6452f1fda8eef8e747e1a29514
benjabe <benjaminbergzh@gmail.com>
2020-04-04 14:35:01 +0200

    Add main menu.

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Button _playButton = null;
+    [SerializeField] private Button _quitButton = null;
+
+    private void Awake()
+    {
+        _playButton.onClick.AddListener(() =>
+        {
+            Debug.Log("Play");
+            SceneManager.LoadScene("OfficeScene");
+        });
+        _quitButton.onClick.AddListener(() =>
+        {
+            Debug.Log("Quit");
+            Application.Quit();
+        });
+
+        SoundManager.PlayMusic("TestMusic");
+    }
+}
