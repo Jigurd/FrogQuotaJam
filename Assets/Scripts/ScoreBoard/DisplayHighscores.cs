30e62e4134672db96250b021d7386beec4f586eb
Mathias Hareide <mathiashareide4497@gmail.com>
2020-04-06 20:46:55 +0200

    Memes

---
+++ Assets/Scripts/ScoreBoard/DisplayHighscores.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayHighscores : MonoBehaviour
+{
+    [SerializeField] private float _highscoreRefreshRate = 30;
+    [SerializeField] private Text[] _highscoreText = null;
+    [SerializeField] private Text _lastScore;
+
+    public static Highscores HighscoreManager;
+
+    private void Awake()
+    {
+        HighscoreManager = GetComponent<Highscores>();
+
+        _lastScore.text = "";
+
+        for (int i = 0; i < _highscoreText.Length; i++)
+        {
+            _highscoreText[i].text = i + 1 + ". Fetching...";
+        }
+
+        StartCoroutine(RefreshHighscores());
+    }
+
+    public void OnHighscoresDownloaded(Highscore[] highscoreList)
+    {
+        for (int i = 0; i < _highscoreText.Length; i++)
+        {
+            _highscoreText[i].text = i + 1 + ". Fetching...";
+            if (highscoreList.Length > i)
+            {
+                _highscoreText[i].text = i + 1 + ". ";
+                _highscoreText[i].text += highscoreList[i].Username + " - " + highscoreList[i].Score;
+            }
+        }
+    }
+    IEnumerator RefreshHighscores()
+    {
+        while (true)
+        {
+            //print("Meme");
+            HighscoreManager.DownloadHighscores();
+            yield return new WaitForSeconds(_highscoreRefreshRate);
+        }
+    }
+    public void ShowYourLatestScore(string username, int score)
+    {
+        _lastScore.text ="Your score: " + username + " - " + score.ToString();
+    }
+}
--- Assets/Scripts/Villains/AyyLmaoManController.cs
+++ Assets/Scripts/Villains/AyyLmaoManController.cs
@@ -6,31 +6,34 @@ public class AyyLmaoManController : MonoBehaviour
 {
     [SerializeField] private float _probeTimer = 0;
     [SerializeField] private float _speed = 0;
+    [SerializeField] private float _probeRange = 1.25f;
     [SerializeField] private GameObject[] _victims = null;
 
-    private float _probeTime = 3;
-    private bool _lookingForVictim = true;
+    // This GameObject needs a collider.
+    private GameObject _victim;
     private Vector3 _pointA;
     private Vector3 _pointB;
     private Vector3 _targetPoint;
-    private GameObject _victim;
-
-    private float _distanceBetweenEnemyAndT(Vector3 v3) 
+    private bool _lookingForVictim = true;
+    private bool _lookingAtVictim = false;
+    private float _probeTime = 3;
+    private float _lineUpStartTime = 0;
+    private float _distanceBetweenEnemyAndT(Vector3 v3)
     {
         return Vector3.Distance(transform.position, v3);
     }
 
     private void Awake()
     {
-        _pointA = new Vector3(transform.position.x + 6, transform.position.y, transform.position.z);
-        _pointB = new Vector3(transform.position.x - 6, transform.position.y, transform.position.z);
+        _pointA = new Vector3(transform.position.x + 4, transform.position.y, transform.position.z);
+        _pointB = new Vector3(transform.position.x - 4, transform.position.y, transform.position.z);
         _targetPoint = _pointA;
     }
     private void Update()
     {
+        //print(Random.Range(0, _victims.Length));
         Move();
-
-        if (!_lookingForVictim)
+        if (_lookingAtVictim)
             Probe();
     }
 
@@ -44,30 +47,39 @@ public class AyyLmaoManController : MonoBehaviour
         else if (_targetPoint == _pointB)
             transform.position += -Vector3.right * _speed * Time.deltaTime;
 
-        if (_distanceBetweenEnemyAndT(_pointA) <= 2)
+        if (_distanceBetweenEnemyAndT(_pointA) <= .1f)
         {
             _targetPoint = _pointB;
         }
-        else if (_distanceBetweenEnemyAndT(_pointB) <= 2)
+        else if (_distanceBetweenEnemyAndT(_pointB) <= .1f)
         {
             _targetPoint = _pointA;
         }
-                
+
         _probeTimer -= Time.deltaTime;
         if (_probeTimer <= 0)
         {
-            _victim = _victims[Random.Range(0, _victims.Length - 1)].gameObject;
+            _victim = _victims[Random.Range(0, _victims.Length)];
+            _lineUpStartTime = Time.time;
 
-            _lookingForVictim = false;
-            print("Came to earth in a silver chrome U.F.O, to probe your ass and see how deep it can go.");
-
-            _probeTimer = _probeTime;
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, Mathf.Infinity);
+            if (hit.collider == _victim.GetComponent<BoxCollider2D>())
+            {
+                _lookingForVictim = false;
+                _probeTimer = _probeTime;
+                StartCoroutine(WaitBeforeProbe());
+            }
         }
     }
     private void Probe()
     {
-
-
+        if (_distanceBetweenEnemyAndT(_victim.transform.position) <= _probeRange)
+        {
+            print("PROBE MEMES HAPPEN HERE");
+            return;
+        }
+        Vector3 newDir = _victim.transform.position - transform.position;
+        transform.position += newDir.normalized * _speed * Time.deltaTime;
         /*
         When I show up, I'm just a little SECTOID.
         But later on, I become a MECHTOID.
@@ -81,5 +93,10 @@ public class AyyLmaoManController : MonoBehaviour
         Gain a boost when I merge my mind with my bro,
         AAAAAAAAAAAAAAAY LMAO. (2x)
         */
+    }
+    private IEnumerator WaitBeforeProbe()
+    {
+        yield return new WaitForSeconds(2);
+        _lookingAtVictim = true;
     }
 }
